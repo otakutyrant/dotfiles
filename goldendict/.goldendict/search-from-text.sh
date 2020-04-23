3d72b5c987bd5fed31d6c0d5425559c7f3526e7c
acgtyrant <acgtyrant@gmail.com>
2020-04-23 16:03:19 +0800

    search-from-text.sh: iff matched, show

--- goldendict/.goldendict/search-from-text.sh
+++ goldendict/.goldendict/search-from-text.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 zotero () {
-  echo "<b>calibre</b>"
+  # TODO: combine all text into a library and maintain it
+  match_flag=false
   pdf_directory=$HOME/.zotero/zotero/yg83zi0h.default/zotero/storage
   tmp_directory=$HOME/tmp/zotero_txt
   sum=0
   count=0
   IFS=$'\n'
   for line in $(find $pdf_directory -name '*.pdf'); do
+    if [ "$match_flag" = false ]; then
+      match_flag=true
+      echo "<b>zotero</b>"
+      echo "<br>"
+    fi
     filename="$(basename "$line" ".pdf").txt"
     text_pathname="$tmp_directory/$filename"
     if [ -f "$text_pathname" ] ; then
@@ -20,14 +26,21 @@ zotero () {
     fi
   done
   unset IFS
-  echo "PDF count: $sum"
-  echo "<br>"
+  if [ "$match_flag" = true ]; then
+    echo "PDF count: $sum"
+    echo "<br>"
+  fi
 }
 
 calibre () {
-  echo "<b>calibre</b>"
   calibre_directory=$HOME/Calibre
+  match_flag=false
   grep --include \*.txt -lr "\<$1\>" $calibre_directory | while read line; do
+    if [ "$match_flag" = false ]; then
+      match_flag=true
+      echo "<b>calibre</b>"
+      echo "<br>"
+    fi
     headline="$(basename "$line" .txt)"
     echo "<details>"
     count="$(grep -c "\<$1\>" "$line")"
@@ -39,19 +52,26 @@ calibre () {
     done
     echo "</details>"
   done
-  echo "<br>"
 }
 
 oxford () {
-  echo "<b>oxford level</b>"
-  grep "\b$1\b" $HOME/.goldendict/oxford5000.txt
-  echo "<br>"
+  grep "\b$1\b" $HOME/.goldendict/oxford5000.txt > /dev/null
+  if [ $? -eq 0 ] ; then
+    echo "<b>oxford level</b>"
+    grep "\b$1\b" $HOME/.goldendict/oxford5000.txt
+    echo "<br>"
+  fi
 }
 
 subtitles () {
-  echo "<b>subtitles</b>"
+  match_flag=false
   subtitles_directory=$HOME/Nutstore/subtitles
   grep -lr "\<$1\>" $subtitles_directory | while read line; do
+    if [ "$match_flag" = false ]; then
+      match_flag=true
+      echo "<b>subtitles</b>"
+      echo "<br>"
+    fi
     filename=`basename $line`
     echo "<details>"
     count="$(grep -c "\<$1\>" "$line")"
