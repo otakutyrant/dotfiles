32a22146b80be3581517e626f2b8582d6dac62eb
acgtyrant <acgtyrant@gmail.com>
2020-05-19 19:23:39 +0800

    ipython_config.py: update for IPthon>=7.0 and powerline 2.8-1
    https://github.com/powerline/powerline/issues/1953#issuecomment-630753400

--- Python/.ipython/profile_default/ipython_config.py
+++ Python/.ipython/profile_default/ipython_config.py
@@ -1,4 +1,2 @@
-from powerline.bindings.ipython.since_5 import PowerlinePrompts
-c = get_config()
-c.TerminalInteractiveShell.simple_prompt = False
+from powerline.bindings.ipython.since_7 import PowerlinePrompts
 c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
