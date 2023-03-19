d4398028bcb86de483f0501d3d4d3e7ee4641820
otakutyrant <otakutyrant@gmail.com>
2023-03-19 15:53:51 +0800

    show_new_words.py: adapted for pre-commit

--- GoldenDict/.goldendict/show_new_words.py
+++ GoldenDict/.goldendict/show_new_words.py
@@ -1,19 +1,22 @@
 #!/bin/env python
 
+import re
 from collections import Counter
 from pathlib import Path
-import re
 
 
 def main():
-    learnt_words_pathname = Path.home() / 'Calibre Library' / 'Learnt Words' / 'lemmas.txt'
+    learnt_words_pathname = (
+        Path.home() / "Calibre Library" / "Learnt Words" / "lemmas.txt"
+    )
     set_ = set()
     with open(learnt_words_pathname) as file_:
         for line in file_:
-            word = re.split(r'\W+', line, 1)[0]
+            word = re.split(r"\W+", line, 1)[0]
             set_.add(word)
-    # pathname = Path('/home/otakutyrant/Calibre Library/Hourly History/The Black Death_ A History From Begi (65)/lemmas.txt')
-    pathname = Path('/home/otakutyrant/Calibre Library/Frank Herbert/Dune (56)/lemmas.txt')
+    pathname = Path(
+        "/home/otakutyrant/Calibre Library/Frank Herbert/Dune (56)/lemmas.txt"
+    )
     counter = Counter()
     with open(pathname) as file_:
         for line in file_:
@@ -21,10 +24,10 @@ def main():
             counter[word] = int(count)
     for word in set_:
         del counter[word]
-    with open('new_words.txt', 'w') as new_words_file:
+    with open("new_words.txt", "w") as new_words_file:
         for word, count in counter.most_common():
-            new_words_file.write(f'{word} {count}\n')
+            new_words_file.write(f"{word} {count}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
