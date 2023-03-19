f5404ab77817f1bbc81b1965924c2d0a54b8e4e5
otakutyrant <otakutyrant@gmail.com>
2023-03-19 12:57:35 +0800

    search_from_text.py: adapted for pre-commit

--- GoldenDict/.goldendict/search_from_text.py
+++ GoldenDict/.goldendict/search_from_text.py
@@ -1,49 +1,55 @@
 #!/bin/env python
 
-from functools import partial
 import itertools
-from pathlib import Path
 import re
 import sys
+from functools import partial
+from pathlib import Path
 
-lemma_pathname = Path.home() / '.goldendict/lemma.en.txt'
+lemma_pathname = Path.home() / ".goldendict/lemma.en.txt"
 lemma_file = open(lemma_pathname)
 
 
 def word_to_inflections(word: str):
     for line in lemma_file:
-        match = re.search(rf'(?=[^-\']|^)\b{word}\b(?=[^-\']|$)', line)
+        match = re.search(rf"(?=[^-\']|^)\b{word}\b(?=[^-\']|$)", line)
         if match:
-            lemma = line.split(' ')[0].split('/')[0]
-            inflections = line.split()[-1].split(',')
+            lemma = line.split(" ")[0].split("/")[0]
+            inflections = line.split()[-1].split(",")
             if lemma not in inflections:
                 inflections.insert(0, lemma)
             else:
                 index = inflections.index(lemma)
-                inflections[0], inflections[index] = inflections[index], inflections[0]
+                inflections[0], inflections[index] = (
+                    inflections[index],
+                    inflections[0],
+                )
             return inflections
     return None
 
 
+def words_to_keyword(words) -> str:
+    return " ".join(words)
+
+
 def search_from_directory(directory: Path, keyword: str, suffix: str) -> str:
     combinations = []
-    words = keyword.split(' ')
+    words = keyword.split(" ")
     for word in words:
         inflections = word_to_inflections(word)
         if inflections is None:
             combinations.append((word,))
         else:
             combinations.append(inflections)
-    keywords = list(itertools.product(*combinations))
+    keywords = [
+        words_to_keyword(words)
+        for words in list(itertools.product(*combinations))
+    ]
+    pattern = "\\b({})\\b".format("|".join(keywords))
+    record = ""
 
-    def words_to_keyword(words):
-        return ' '.join(words)
-    keywords = [words_to_keyword(words) for words in keywords]
-    pattern = '\\b({})\\b'.format('|'.join(keywords))
-    record = ''
-
-    for pathname in directory.rglob(f'*.{suffix}'):
-        if 'lemma' in pathname.name or 'deck' in pathname.name:
+    for pathname in directory.rglob(f"*.{suffix}"):
+        if "lemma" in pathname.name or "deck" in pathname.name:
             continue
         lines = []
         with open(pathname) as file_:
@@ -52,25 +58,28 @@ def search_from_directory(directory: Path, keyword: str, suffix: str) -> str:
                     lines.append(line)
         count = len(lines)
         if count > 0:
-            record += '<details>'
-            record += f'<summary>{pathname.stem} <b>{count}</b></summary>'
+            record += "<details>"
+            record += f"<summary>{pathname.stem} <b>{count}</b></summary>"
             for line in lines:
-                record += re.sub(f'({pattern})', r'<b>\1</b>', line).strip()
-                record += '<br>'
-                record += '<br>'
-            record += '</details>'
-    if record != '':
-        record = f"""
+                record += re.sub(f"({pattern})", r"<b>\1</b>", line).strip()
+                record += "<br>"
+                record += "<br>"
+            record += "</details>"
+    if record != "":
+        record = (
+            f"""
         <b>{directory.stem}</b>
         <br>
-        """ + record
+        """
+            + record
+        )
     return record
 
 
-calibre_directory = Path.home() / 'Calibre Library'
-calibre = partial(search_from_directory, calibre_directory, suffix='txt')
-subtitles_directory = Path.home() / 'Nutstore/subtitles'
-subtitles = partial(search_from_directory, subtitles_directory, suffix='srt')
+calibre_directory = Path.home() / "Calibre Library"
+calibre = partial(search_from_directory, calibre_directory, suffix="txt")
+subtitles_directory = Path.home() / "Nutstore/subtitles"
+subtitles = partial(search_from_directory, subtitles_directory, suffix="srt")
 
 
 def main():
@@ -84,10 +93,10 @@ def main():
         record += function(keyword)
     record += """</body>
 </html>"""
-    line_numbers = len(record.split('\n'))
+    line_numbers = len(record.split("\n"))
     if line_numbers > 5:
         print(record)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
