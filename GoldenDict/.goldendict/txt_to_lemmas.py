79a52e5a26fe66be67a3b30fd606c4320f040236
otakutyrant <otakutyrant@gmail.com>
2023-03-19 20:02:07 +0800

    txt_to_lemmas.py: improve filter logics

--- GoldenDict/.goldendict/txt_to_lemmas.py
+++ GoldenDict/.goldendict/txt_to_lemmas.py
@@ -4,6 +4,27 @@ from collections import Counter
 from pathlib import Path
 
 
+def filter_to_valid_words(file_):
+    words = [
+        word
+        for line in file_
+        for word in line.split()
+        if word.isalpha() and word.isascii()
+    ]
+    return words
+
+
+def discard_duplicate_words(words):
+    set_ = set(words)
+    words = [word.lower() if word.lower() in set_ else word for word in words]
+    return words
+
+
+def discard_capital_words(words):
+    words = [word for word in words if word.islower()]
+    return words
+
+
 def main():
     hashmap = {}
     lemma_pathname = Path.home() / ".goldendict/lemma.en.txt"
@@ -16,29 +37,20 @@ def main():
             for inflection in inflections:
                 hashmap[inflection] = lemma
     for pathname in (Path.home() / "Calibre Library").glob("**/*.txt"):
-        if pathname.name == "lemmas.txt":
-            continue
-        lemma_pathname = pathname.parent / "lemmas.txt"
-        with open(pathname) as file_, open(lemma_pathname, "w") as lemma_file:
-            words = [
-                word
-                for line in file_
-                for word in line.split()
-                if word.isalpha() and word.isascii()
-            ]
-            set_ = set(words)
-            words = [
-                word.lower() if word.lower() in set_ else word
-                for word in words
-            ]
-            lemmas = [
-                hashmap[word.lower()] if word.lower() in hashmap else word
-                for word in words
-            ]
-            counter = Counter(lemmas)
-            for word, count in counter.most_common():
-                lemma_file.write(f"{word} {count}\n")
-        print(f"{pathname} done.")
+        if pathname.name != "lemmas.txt":
+            lemma_pathname = pathname.parent / "lemmas.txt"
+            with open(pathname) as file_, open(
+                lemma_pathname, "w"
+            ) as lemma_file:
+                words = filter_to_valid_words(file_)
+                words = discard_duplicate_words(words)
+                words = discard_capital_words(words)
+
+                lemmas = [hashmap.get(word, word) for word in words]
+                counter = Counter(lemmas)
+                for word, count in counter.most_common():
+                    lemma_file.write(f"{word} {count}\n")
+            print(f"{pathname} done.")
 
 
 if __name__ == "__main__":
