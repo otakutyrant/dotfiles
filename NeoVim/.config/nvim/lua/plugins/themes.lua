f2f1370d293313e05ecebe6d5745f828e556e55c
otakutyrant <otakutyrant@gmail.com>
2023-06-20 19:51:00 +0800

    themes.lua: use monokai instead

--- NeoVim/.config/nvim/lua/plugins/themes.lua
+++ NeoVim/.config/nvim/lua/plugins/themes.lua
@@ -1,15 +1,13 @@
 return {
     {
-        "folke/tokyonight.nvim",
+        "tanvirtin/monokai.nvim",
         lazy = false,
         priority = 1000,
         opts = {},
         config = function()
             -- Enable 24-bit RGB color in the TUI, which is better than the tranditional 256 term colors.
             vim.opt.termguicolors = true
-            -- Use tokyonight theme.
-            vim.cmd.colorscheme('tokyonight')
+            vim.cmd.colorscheme("monokai")
         end,
     }
 }
-
