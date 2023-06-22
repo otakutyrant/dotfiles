25e8c952907f5133a0d380bc2c1020ee7e9200ae
otakutyrant <otakutyrant@gmail.com>
2023-06-22 17:34:57 +0800

    search.lua: use telescope instead of fzf

--- NeoVim/.config/nvim/lua/plugins/search.lua
+++ NeoVim/.config/nvim/lua/plugins/search.lua
@@ -1,7 +1,24 @@
+-- A highly extendable fuzzy finder over lists.
+telescope = {
+    'nvim-telescope/telescope.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+        { 'nvim-lua/plenary.nvim' }, -- Library API.
+        -- An fzf extension for telescope, writen in C.
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+        -- Get fzf loaded and working with telescope.
+        require('telescope').load_extension('fzf')
+        -- The official recommended keymaps for telescope.
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+}
+
 return {
-    {
-        "junegunn/fzf",
-        name = "fzf",
-        build = "./install --all",
-    }
+    telescope,
 }
