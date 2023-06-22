9637b8522ee313f062536503bb4f2cae6c3e525a
otakutyrant <otakutyrant@gmail.com>
2023-06-22 22:26:27 +0800

    wordlist.py: isort and black

--- GoldenDict/.goldendict/wordlist/wordlist.py
+++ GoldenDict/.goldendict/wordlist/wordlist.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python
 
+import sys
 from collections import defaultdict
 from pathlib import Path
-import sys
 
 import sh
 
-
-oxford_pathname = Path.home() / '.goldendict/wordlist/oxford5000.txt'
-lemma_pathname = Path.home() / '.goldendict/lemma.en.txt'
+oxford_pathname = Path.home() / ".goldendict/wordlist/oxford5000.txt"
+lemma_pathname = Path.home() / ".goldendict/lemma.en.txt"
 
 
 def word_to_lemma(word: str):
     result = None
     try:
-        _ = sh.egrep(f'([^-\']|^){word}([^-\']|$)', str(lemma_pathname))
-        result = sh.egrep(f'\\b{word}\\b', _in=_)
+        _ = sh.egrep(f"([^-']|^){word}([^-']|$)", str(lemma_pathname))
+        result = sh.egrep(f"\\b{word}\\b", _in=_)
     except sh.ErrorReturnCode_1:
         return None
     else:
-        if '/' in result:
-            lemma = result.split(' ')[0].split('/')[0]
+        if "/" in result:
+            lemma = result.split(" ")[0].split("/")[0]
         else:
-            lemma = result.split(' ')[0]
+            lemma = result.split(" ")[0]
         return lemma
 
 
 def oxford(keyword: str) -> str:
     try:
         record = sh.egrep(
-                f'\\b{keyword}\\b', str(oxford_pathname)
+            f"\\b{keyword}\\b", str(oxford_pathname)
         ).stdout.decode()
     except sh.ErrorReturnCode_1:
         return ""
@@ -45,14 +44,14 @@ def main():
     if lemma is not None:
         keyword = lemma
     print(oxford(keyword))
-    for pathname in directory.glob('*.txt'):
+    for pathname in directory.glob("*.txt"):
         if pathname.name != oxford_pathname.name:
             with open(pathname) as file_:
                 for line in file_:
                     words[line.strip()].append(str(pathname.stem))
     if keyword in words.keys():
-        print('\n'.join(words[keyword]))
+        print("\n".join(words[keyword]))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
