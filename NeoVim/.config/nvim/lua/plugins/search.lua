43d62322b482ff294eda48a0b2d34c3f427140c0
otakutyrant <otakutyrant@gmail.com>
2023-06-22 21:58:53 +0800

    search.lua: enable search hidden files

--- NeoVim/.config/nvim/lua/plugins/search.lua
+++ NeoVim/.config/nvim/lua/plugins/search.lua
@@ -16,8 +16,19 @@ telescope = {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        -- File and text search in hidden files and directories
+        --https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
+        local telescopeConfig = require("telescope.config")
+        -- Clone the default Telescope configuration
+        local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+        -- I want to search in hidden/dot files.
+        table.insert(vimgrep_arguments, "--hidden")
+        -- I don't want to search in the `.git` directory.
+        table.insert(vimgrep_arguments, "--glob")
+        table.insert(vimgrep_arguments, "!**/.git/*")
     end
 }
+
 
 return {
     telescope,
