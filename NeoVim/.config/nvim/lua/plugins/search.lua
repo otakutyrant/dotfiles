99df95470b1be2b1c7240b1fda9013ba31de1562
otakutyrant <otakutyrant@gmail.com>
2023-06-24 14:10:54 +0800

    search.lua: fix wrong setup for searching hidden

--- NeoVim/.config/nvim/lua/plugins/search.lua
+++ NeoVim/.config/nvim/lua/plugins/search.lua
@@ -18,6 +18,7 @@ telescope = {
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
         -- File and text search in hidden files and directories
         --https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
+        local telescope = require("telescope")
         local telescopeConfig = require("telescope.config")
         -- Clone the default Telescope configuration
         local vimgrep_arguments =
@@ -27,6 +28,24 @@ telescope = {
         -- I don't want to search in the `.git` directory.
         table.insert(vimgrep_arguments, "--glob")
         table.insert(vimgrep_arguments, "!**/.git/*")
+        telescope.setup({
+            defaults = {
+                -- `hidden = true` is not supported in text grep commands.
+                vimgrep_arguments = vimgrep_arguments,
+            },
+            pickers = {
+                find_files = {
+                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+                    find_command = {
+                        "rg",
+                        "--files",
+                        "--hidden",
+                        "--glob",
+                        "!**/.git/*",
+                    },
+                },
+            },
+        })
     end,
 }
 
