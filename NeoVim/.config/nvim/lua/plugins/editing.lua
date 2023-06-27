b18faec36e7aa6cf8e62c4af6e4de5555c184be3
otakutyrant <otakutyrant@gmail.com>
2023-06-27 17:43:52 +0800

    editing.lua: new comment plugin

---
+++ NeoVim/.config/nvim/lua/plugins/editing.lua
@@ -0,0 +1,13 @@
+local comment = {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+    config = function()
+        require("Comment").setup({
+            pre_hook = require(
+                "ts_context_commentstring.integrations.comment_nvim"
+            ).create_pre_hook(),
+        })
+    end,
+}
+return { comment }
