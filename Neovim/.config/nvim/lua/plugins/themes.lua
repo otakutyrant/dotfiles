1cad6d99058d5938003898ec2c1c7c03b2b9b0d1
otakutyrant <otakutyrant@gmail.com>
2023-07-04 09:15:06 +0800

    themes.lua: add tokyonight and make themes random

--- Neovim/.config/nvim/lua/plugins/themes.lua
+++ Neovim/.config/nvim/lua/plugins/themes.lua
@@ -1,13 +1,42 @@
-return {
-    {
-        "tanvirtin/monokai.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {},
-        config = function()
-            -- Enable 24-bit RGB color in the TUI, which is better than the tranditional 256 term colors.
-            vim.opt.termguicolors = true
-            vim.cmd.colorscheme("monokai")
-        end,
-    },
+local tokyonight = {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    name = "tokyonight",
 }
+
+-- tokyonight
+-- tokyonight-night
+-- tokyonight-strom
+-- tokyonight-day
+-- tokyonight-moon
+
+local monokai = {
+    "tanvirtin/monokai.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    name = "monokai",
+}
+
+local function enable_colorscheme(theme)
+    local wrapped_function = function()
+        local theme_name = theme.name
+        vim.cmd.colorscheme(theme_name)
+    end
+    return wrapped_function
+end
+
+local function getRandomElement(list)
+    math.randomseed(os.time())
+    -- Generate a random index within the range of the list size.
+    local index = math.random(#list)
+    return list[index]
+end
+
+local themes = { tokyonight, monokai }
+local theme = getRandomElement(themes)
+theme.config = enable_colorscheme(theme)
+
+return themes
