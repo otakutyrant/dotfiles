f17345899b58c7392d562ad322556ddc1a247dc6
otakutyrant <otakutyrant@gmail.com>
2023-07-04 10:27:52 +0800

    themes.lua: enable more random themes from one theme plugin

--- Neovim/.config/nvim/lua/plugins/themes.lua
+++ Neovim/.config/nvim/lua/plugins/themes.lua
@@ -3,30 +3,27 @@ local tokyonight = {
     lazy = false,
     priority = 1000,
     opts = {},
-    name = "tokyonight",
+    names = {
+        "tokyonight",
+        "tokyonight-night",
+        "tokyonight-strom",
+        "tokyonight-day",
+        "tokyonight-moon",
+    },
 }
-
--- tokyonight
--- tokyonight-night
--- tokyonight-strom
--- tokyonight-day
--- tokyonight-moon
 
 local monokai = {
     "tanvirtin/monokai.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-    name = "monokai",
+    names = {
+        "monokai",
+        "monokai_pro",
+        "monokai_soda",
+        "monokai_ristretto",
+    },
 }
-
-local function enable_colorscheme(theme)
-    local wrapped_function = function()
-        local theme_name = theme.name
-        vim.cmd.colorscheme(theme_name)
-    end
-    return wrapped_function
-end
 
 local function getRandomElement(list)
     math.randomseed(os.time())
@@ -35,11 +32,19 @@ local function getRandomElement(list)
     return list[index]
 end
 
+local function enable_colorscheme(theme_name)
+    local wrapped_function = function()
+        vim.cmd.colorscheme(theme_name)
+    end
+    return wrapped_function
+end
+
 local themes = {
     tokyonight,
     monokai,
 }
 local theme = getRandomElement(themes)
-theme.config = enable_colorscheme(theme)
+local theme_name = getRandomElement(theme.names)
+theme.config = enable_colorscheme(theme_name)
 
 return themes
