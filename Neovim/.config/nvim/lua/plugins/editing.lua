de50d5de696d49758cc90195dafdb92d736a57cd
otakutyrant <otakutyrant@gmail.com>
2023-07-04 09:56:47 +0800

    editing.lua: add nvim-surround

--- Neovim/.config/nvim/lua/plugins/editing.lua
+++ Neovim/.config/nvim/lua/plugins/editing.lua
@@ -11,6 +11,20 @@ local comment = {
     end,
 }
 
+local surround = {
+    "kylechui/nvim-surround",
+    dependendcies = {
+        "nvim-treesitter",
+        "nvim-treesitter-textobjects",
+    },
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features.
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({})
+    end,
+}
+
 return {
     comment,
+    surround,
 }
