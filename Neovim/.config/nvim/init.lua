a388de86f7491c1f5e5433611e50b0dae9ef6de7
otakutyrant <otakutyrant@gmail.com>
2023-07-18 12:21:48 +0800

    two basic configs moved to lua directory

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -1,2 +1,4 @@
-require("config.editor")
-require("config.plugin")
+-- These modules are imported from `Lua` directory.
+require("editor")
+require("plugin_manager")
+-- All plugins are put in lua/plugins and managed by the plugin manager.
