8a20aa1548a9bc101a9de4226148bbd2247a6d4b
otakutyrant <otakutyrant@gmail.com>
2023-08-04 09:42:26 +0800

    use telescope to live grep the current word now

--- Neovim/.config/nvim/lua/plugins/search.lua
+++ Neovim/.config/nvim/lua/plugins/search.lua
@@ -14,6 +14,7 @@ local telescope = {
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>fw", builtin.grep_string, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
         -- File and text search in hidden files and directories
