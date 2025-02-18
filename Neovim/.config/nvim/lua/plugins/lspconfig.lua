d17a7e90ae1cf44a465d91924d05f672bc54f8e4
otakutyrant <otakutyrant@gmail.com>
2025-02-18 21:53:32 +0800

    refactor

--- Neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ Neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -1,84 +1,62 @@
--- lspconfig offers a unified way to use language servers and obtain features like diagnostics and complementation.
+-- lspconfig offers an unified way to use language servers and obtain features like diagnostics and completion.
 
-local lua_ls_setup = {
-    settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = { "vim" },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-}
-
--- Configuration settings for the nvim-lspconfig plugin
 local nvim_lspconfig = {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.pyright.setup({})
-        lspconfig.lua_ls.setup(lua_ls_setup)
-        lspconfig.bashls.setup({})
-    end,
 }
 
--- This snippet is copied from https://github.com/neovim/nvim-lspconfig#suggested-configuration
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
--- This keymap confits with quick quit. TODO: find an alternative keymap.
--- vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+-- # Language Server setups
+-- Here are setups for some language servers, and all of them are used in lspconfig.
 
--- Use LspAttach autocommand to only map the following keys
--- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-    callback = function(ev)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+-- Firstly, fetch lspconfig to add setups.
+local lspconfig = require("lspconfig")
 
-        -- Buffer local mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-        vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set(
-            "n",
-            "<space>wr",
-            vim.lsp.buf.remove_workspace_folder,
-            opts
-        )
-        vim.keymap.set("n", "<space>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "<space>f", function()
-            vim.lsp.buf.format({ async = true })
-        end, opts)
+-- Lua
+-- This setup make lua-language-server handles Neovim lua-related config and even plugins code.
+-- It is copied from https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls,
+-- without me understanding it fully. Note that I replaced `vim.loop` with `vim.uv` since the former is deprecated in Neovim 0.10
+lspconfig.lua_ls.setup({
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc')) then
+                return
+            end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    -- Solve the issue about undefined `vim.uv.fs_stat`
+                    -- See https://github.com/folke/lazy.nvim/discussions/1349#discussioncomment-9122673
+                    "${3rd}/luv/library"
+                    -- "${3rd}/busted/library",
+                }
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                -- library = vim.api.nvim_get_runtime_file("", true)
+            }
+        })
     end,
+    settings = {
+        Lua = {}
+    }
 })
+
+-- Python
+lspconfig.pyright.setup({})
+
+-- TODO: To exploit lsp fully, like vim.lsp.buf.declaration and vim.lsp.buf.definition
+-- Use LspAttach autocommand to map some lsp-buf functions.
+
+-- # Final
 
 return {
     nvim_lspconfig,
