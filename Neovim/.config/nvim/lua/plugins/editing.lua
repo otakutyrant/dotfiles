39747f8c27f2ff462a2344efeccf8d1c15a37b06
otakutyrant <otakutyrant@gmail.com>
2025-02-19 16:24:37 +0800

    simplified

--- Neovim/.config/nvim/lua/plugins/editing.lua
+++ Neovim/.config/nvim/lua/plugins/editing.lua
@@ -1,14 +1,6 @@
 local comment = {
     "numToStr/Comment.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
-    config = function()
-        require("Comment").setup({
-            pre_hook = require(
-                "ts_context_commentstring.integrations.comment_nvim"
-            ).create_pre_hook(),
-        })
-    end,
 }
 
 local surround = {
