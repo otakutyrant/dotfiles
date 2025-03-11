cc127cd792fa2a87306bf159e375c2a61401883f
otakutyrant <otakutyrant@gmail.com>
2025-03-12 05:31:28 +0800

    add web development tools

--- Neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ Neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -19,16 +19,16 @@ lspconfig.lua_ls.setup({
     on_init = function(client)
         if client.workspace_folders then
             local path = client.workspace_folders[1].name
-            if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc')) then
+            if path ~= vim.fn.stdpath("config") and (vim.uv.fs_stat(path.."/.luarc.json") or vim.uv.fs_stat(path.."/.luarc.jsonc")) then
                 return
             end
         end
 
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
             runtime = {
-                -- Tell the language server which version of Lua you're using
+                -- Tell the language server which version of Lua you"re using
                 -- (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT'
+                version = "LuaJIT"
             },
             -- Make the server aware of Neovim runtime files
             workspace = {
@@ -40,7 +40,7 @@ lspconfig.lua_ls.setup({
                     "${3rd}/luv/library"
                     -- "${3rd}/busted/library",
                 }
-                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                -- or pull in all of "runtimepath". NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
                 -- library = vim.api.nvim_get_runtime_file("", true)
             }
         })
@@ -61,6 +61,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 lspconfig.html.setup({ capabilities = capabilities })
 lspconfig.cssls.setup({ capabilities = capabilities })
+lspconfig.eslint.setup({})
 
 -- TypeScript & JavaScript
 -- Actually this is not a language server.
