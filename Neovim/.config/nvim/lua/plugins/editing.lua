96f2cab504bc1e94d525c87c49d2c35811e9ccd0
otakutyrant <otakutyrant@gmail.com>
2025-03-14 23:21:32 +0800

    add autotag and conform

--- Neovim/.config/nvim/lua/plugins/editing.lua
+++ Neovim/.config/nvim/lua/plugins/editing.lua
@@ -9,7 +9,39 @@ local surround = {
     event = "VeryLazy",
 }
 
+local autotag = {
+    "windwp/nvim-ts-autotag", config = true,
+}
+
+-- formatter
+local conform = {
+  'stevearc/conform.nvim',
+  config = function()
+      require("conform").setup({
+          formatters_by_ft = {
+              html = { 'prettier' },
+              css = { 'prettier' },
+              json = { 'prettier' },
+              -- deno = { 'prettier' },
+              yaml = { 'prettier' },
+              markdown = { 'prettier' },
+              typescript = { 'prettier' },
+              -- ocaml = { 'ocamlformat' },
+              -- nix = { 'nixpkgs-fmt' },
+              -- python = { 'ruff_format', 'ruff_organaize_imports', 'ruff_fix' },
+          },
+          format_on_save = {
+              -- These options will be passed to conform.format()
+              timeout_ms = 500,
+              lsp_format = "fallback",
+          },
+      })
+  end,
+}
+
 return {
     comment,
     surround,
+    autotag,
+    conform,
 }
