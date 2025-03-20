e3eff2a15a11686562f7c768d7e8f0b3e394d005
otakutyrant <otakutyrant@gmail.com>
2025-03-20 18:30:29 +0800

    format!

--- Neovim/.config/nvim/lua/plugins/editing.lua
+++ Neovim/.config/nvim/lua/plugins/editing.lua
@@ -1,10 +1,12 @@
 local comment = {
-    "numToStr/Comment.nvim", config = true,
+    "numToStr/Comment.nvim",
+    config = true,
     event = { "BufReadPre", "BufNewFile" },
 }
 
 local surround = {
-    "kylechui/nvim-surround", config = true,
+    "kylechui/nvim-surround",
+    config = true,
     version = "*", -- Use for stability; omit to use `main` branch for the latest features.
     event = "VeryLazy",
 }
@@ -15,28 +17,28 @@ local autotag = {
 
 -- formatter
 local conform = {
-  'stevearc/conform.nvim',
-  config = function()
-      require("conform").setup({
-          formatters_by_ft = {
-              html = { 'prettier' },
-              css = { 'prettier' },
-              json = { 'prettier' },
-              -- deno = { 'prettier' },
-              yaml = { 'prettier' },
-              markdown = { 'prettier' },
-              typescript = { 'prettier' },
-              -- ocaml = { 'ocamlformat' },
-              -- nix = { 'nixpkgs-fmt' },
-              -- python = { 'ruff_format', 'ruff_organaize_imports', 'ruff_fix' },
-          },
-          format_on_save = {
-              -- These options will be passed to conform.format()
-              timeout_ms = 500,
-              lsp_format = "fallback",
-          },
-      })
-  end,
+    'stevearc/conform.nvim',
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                html = { 'prettier' },
+                css = { 'prettier' },
+                json = { 'prettier' },
+                -- deno = { 'prettier' },
+                yaml = { 'prettier' },
+                markdown = { 'prettier' },
+                typescript = { 'prettier' },
+                -- ocaml = { 'ocamlformat' },
+                -- nix = { 'nixpkgs-fmt' },
+                -- python = { 'ruff_format', 'ruff_organaize_imports', 'ruff_fix' },
+            },
+            format_on_save = {
+                -- These options will be passed to conform.format()
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+        })
+    end,
 }
 
 return {
