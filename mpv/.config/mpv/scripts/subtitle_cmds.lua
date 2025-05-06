f57acffc9586ce7b9989bdcfc19dfd12baec5023
otakutyrant <otakutyrant@gmail.com>
2025-05-06 19:23:07 +0800

    improve ab-loop shortcuts

--- mpv/.config/mpv/scripts/subtitle_cmds.lua
+++ mpv/.config/mpv/scripts/subtitle_cmds.lua
@@ -1,71 +1,28 @@
--- Based on https://github.com/perogiue/mpv-scripts
---
--- ___Usage___:
---- ab-loop-sub [autopause]: Set ab-loop to current subtitle. Specify 'autopause' arg to pause before every loop
---- copy-subtitle:           Copy current subtitle to clipboard. Works on mac 10.13 and windows 10
+---@class mp
+---@field add_key_binding fun(key: string, name: string, fn: fun())
+---@field get_property_native fun(name: string): any
+---@field set_property_native fun(name: string, value: any)
+---@field osd_message fun(message: string, duration?: number)
+---@diagnostic disable-next-line: lowercase-global
+---@type mp
+mp = mp
 
-_G.abloopavoidjankpause = false
+mp.add_key_binding("a", "ab-loop-set-a", function()
+    mp.set_property_native("ab-loop-a", "no")
+    local time_pos = mp.get_property_native("time-pos")
+    mp.set_property_native("ab-loop-a", time_pos)
+    mp.osd_message("set ab-loop-a")
+end)
 
-function pause_on_sub_loop(prop, value)
-    if _G.abloopavoidjankpause == true then
-        _G.abloopavoidjankpause = false
-        return
-    end
-    if value and value ~= "" then
-        local startloop = mp.get_property("ab-loop-a")
-        local substart = mp.get_property("sub-start")
-        if substart and startloop and substart == startloop then
-            mp.set_property("pause", "yes")
-        end
-    end
-end
+mp.add_key_binding("s", "ab-loop-set-b", function()
+    mp.set_property_native("ab-loop-b", "no")
+    local time_pos = mp.get_property_native("time-pos")
+    mp.set_property_native("ab-loop-b", time_pos)
+    mp.osd_message("set ab-loop-b")
+end)
 
-function ab_loop_sub(autopause)
-    local existingloop = mp.get_property_number("ab-loop-b")
-    if existingloop then
-        mp.osd_message("Clear A-B Loop", 0.5)
-
-        mp.set_property("ab-loop-a", "no")
-        mp.set_property("ab-loop-b", "no")
-
-        mp.unobserve_property(pause_on_sub_loop)
-        mp.set_property("pause", "no")
-    else
-        local substart = mp.get_property("sub-start")
-        local subend = mp.get_property("sub-end")
-
-        if substart and subend then
-            mp.osd_message("A-B Loop Subtitle", 0.5)
-
-            local suboffset = mp.get_property("sub-delay")
-            mp.set_property_number("ab-loop-a", substart + suboffset)
-            mp.set_property_number("ab-loop-b", subend + suboffset + 0.075)
-
-            _G.abloopavoidjankpause = true
-            if autopause then
-                mp.observe_property("sub-text", "native", pause_on_sub_loop)
-            end
-        else
-            mp.osd_message("No subtitles present", 0.5)
-        end
-    end
-end
-
-function escape(s)
-    return (s:gsub("'", "'\\''"))
-end
-
-function copy_subtitle()
-    local subtext = mp.get_property("sub-text")
-    if subtext and subtext ~= "" then
-        os.execute(
-            "echo '" .. escape(subtext) .. "' | xclip -selection clipboard -i"
-        )
-        mp.osd_message("subtitles copied", 0.5)
-    else
-        mp.osd_message("No subtitles present", 0.5)
-    end
-end
-
-mp.add_key_binding(nil, "ab-loop-sub", ab_loop_sub)
-mp.add_key_binding(nil, "copy-subtitle", copy_subtitle)
+mp.add_key_binding("d", "ab-loop-clear", function()
+    mp.set_property_native("ab-loop-a", "no")
+    mp.set_property_native("ab-loop-b", "no")
+    mp.osd_message("clear ab-loop")
+end)
