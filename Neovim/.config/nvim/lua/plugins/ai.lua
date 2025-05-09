828d453bfdd6d1b726543c4bc67a71051daf6624
otakutyrant <otakutyrant@gmail.com>
2025-05-10 01:01:16 +0800

    deprecate chatgpt

--- Neovim/.config/nvim/lua/plugins/ai.lua
+++ Neovim/.config/nvim/lua/plugins/ai.lua
@@ -1,23 +1,3 @@
--- Lazy
-require("os")
-
-local chatgpt = {
-    "jackMort/ChatGPT.nvim",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope.nvim",
-    },
-    event = "VeryLazy",
-    opts = {
-        -- Secret Management.
-        -- https://github.com/jackMort/ChatGPT.nvim#secrets-management
-        api_key_cmd = "cat "
-            .. os.getenv("HOME")
-            .. "/.config/nvim/chatgpt_api.txt",
-    },
-}
-
 local avante = {
     "yetone/avante.nvim",
     event = "VeryLazy",
