c63a73f6b1c1944466ea2c530ffc49bcd163ce96
otakutyrant <otakutyrant@gmail.com>
2025-06-03 14:08:51 +0800

    disable inlayhint

--- Neovim/.config/nvim/lua/editor.lua
+++ Neovim/.config/nvim/lua/editor.lua
@@ -317,7 +317,6 @@ vim.keymap.set("n", "<F3>", function()
     vim.cmd("lclose") -- Close Location List
 end, { desc = "Close Quickfix/Location List" })
 
-vim.lsp.inlay_hint.enable()
 vim.keymap.set("n", "<F4>", function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { desc = "🛠️ Toggle inlay hints" })
