82dc1702b2026e4224ce7e28f0408fa5f8dbcd7d
otakutyrant <otakutyrant@gmail.com>
2025-06-08 21:47:43 +0800

    enable html ls

--- Neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ Neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -119,6 +119,16 @@ vim.lsp.config("nushell", {})
 vim.lsp.enable("taplo")
 vim.lsp.config("taplo", {})
 
+-- HTML
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+vim.lsp.config("html", {
+    capabilities = capabilities,
+})
+vim.lsp.enable("html")
+
 -- # Final
 
 return {
