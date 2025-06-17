7768ab9221dd7152307f91b66d3399e21923bdd0
otakutyrant <otakutyrant@gmail.com>
2025-06-17 21:59:17 +0800

    simplify telescope config and reformat some keys code

--- Neovim/.config/nvim/lua/plugins/search.lua
+++ Neovim/.config/nvim/lua/plugins/search.lua
@@ -8,44 +8,34 @@ local telescope = {
     config = function()
         -- Get fzf loaded and working with telescope.
         require("telescope").load_extension("fzf")
-        -- File and text search in hidden files and directories
-        --https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes#file-and-text-search-in-hidden-files-and-directories
-        local telescope = require("telescope")
-        local telescopeConfig = require("telescope.config")
-        -- Clone the default Telescope configuration
-        local vimgrep_arguments =
-            { unpack(telescopeConfig.values.vimgrep_arguments) }
-        -- I want to search in hidden/dot files.
-        table.insert(vimgrep_arguments, "--hidden")
-        -- I don't want to search in the `.git` directory.
-        table.insert(vimgrep_arguments, "--glob")
-        table.insert(vimgrep_arguments, "!**/.git/*")
-        telescope.setup({
-            defaults = {
-                -- `hidden = true` is not supported in text grep commands.
-                vimgrep_arguments = vimgrep_arguments,
-            },
-            pickers = {
-                find_files = {
-                    -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-                    find_command = {
-                        "rg",
-                        "--files",
-                        "--hidden",
-                        "--glob",
-                        "!**/.git/*",
-                    },
-                },
-            },
-        })
     end,
     -- The official recommended keymaps for telescope.
     keys = {
-        { "<leader>ff", require("telescope.builtin").find_files, { desc = "🛠️ Telescope find files"} },
-        { "<leader>fg", require("telescope.builtin").live_grep, { desc = "🛠️ Telescope live grep"} },
-        { "<leader>fb", require("telescope.builtin").buffers, { desc = "🛠️ Telescope buffers"} },
-        { "<leader>fh", require("telescope.builtin").help_tags, { desc = "🛠️ Telescope find help tags"}},
-        { "<leader>fr", require("telescope.builtin").registers, { desc = "🛠️ Telescope registers"} },
+        {
+            "<leader>ff",
+            require("telescope.builtin").find_files,
+            { desc = "🛠️ Telescope find files" },
+        },
+        {
+            "<leader>fg",
+            require("telescope.builtin").live_grep,
+            { desc = "🛠️ Telescope live grep" },
+        },
+        {
+            "<leader>fb",
+            require("telescope.builtin").buffers,
+            { desc = "🛠️ Telescope buffers" },
+        },
+        {
+            "<leader>fh",
+            require("telescope.builtin").help_tags,
+            { desc = "🛠️ Telescope find help tags" },
+        },
+        {
+            "<leader>fr",
+            require("telescope.builtin").registers,
+            { desc = "🛠️ Telescope registers" },
+        },
     },
 }
 -- Highlight and number the result when you use f or F to search a char.
