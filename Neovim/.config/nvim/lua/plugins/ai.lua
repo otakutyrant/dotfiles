25cf298ccd48a3aa87fbf6297660fa3d0ce65ab9
otakutyrant <otakutyrant@gmail.com>
2025-09-20 06:14:16 +0800

    use openrouter gemini key

--- Neovim/.config/nvim/lua/plugins/ai.lua
+++ Neovim/.config/nvim/lua/plugins/ai.lua
@@ -2,7 +2,18 @@ local avante = {
     "yetone/avante.nvim",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"!
-    opts = { provider = "gemini" },
+    opts = {
+        -- provider = "gemini",
+        provider = "openrouter",
+        providers = {
+            openrouter = {
+                __inherited_from = "openai",
+                endpoint = "https://openrouter.ai/api/v1",
+                api_key_name = "OPENROUTER_API_KEY",
+                model = "x-ai/grok-code-fast-1",
+            },
+        },
+    },
     build = "make",
     dependencies = {
         "stevearc/dressing.nvim",
